5cf3fcf5e64de5d852a0ab79a1c77809edeb662e
jaki <76455103+jakineru@users.noreply.github.com>
2025-07-29 21:34:21 -0300

    up

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-            const API_URL = 'https://24b69cd5c1b5.ngrok-free.app';
+            const API_URL = '127.0.0.1:5000';
 
             const ciInput = document.getElementById('cedula');
             const nombreInput = document.getElementById('nombre');
